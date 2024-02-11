2dc2a58107e6d22b6600652b57fcf868de6056b8
abubakar200 <sheikhabubakr4@gmail.com>
2024-02-11 15:29:27 +0500

    new

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,6 +1,10 @@
+import { useForm } from "react-hook-form";
 import CardWrapper from "./card-wrapper";
-
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
 const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>()
   return (
     <CardWrapper
       headerLabel="Welcome back"
